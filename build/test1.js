b137f1886bca3bab798aff744196b154875814f0
FrederikKofoed <fk.kofoed@gmail.com>
2018-06-11 09:14:13 +0200

    js opdateret

--- build/test1.js
+++ build/test1.js
@@ -40,30 +40,41 @@ circleStyling();
 };
 
 
-function circleStyling(){
 
-// find circle 1
-    let circle1 = document.querySelector(".circle1");
+function circleStyling() {
 
-// lav condition til skift farve på cirkel 1
-    if (queueData < 5){
+    let circle1 = document.querySelector("#queue-light")
+
+    if (queueData < 5) {
         circle1.style.background = "#87ab66";
-    } else if (queueData < 10 ){
+        circle1.classList.remove("neon-yellow", "neon-red");
+        circle1.classList.add("neon-green");
+
+    } else if (queueData < 10) {
         circle1.style.background = "#e79d3f";
-    } else if (queueData < 15 ){
+        circle1.classList.remove("neon-green", "neon-red");
+        circle1.classList.add("neon-yellow");
+
+    } else if (queueData < 15) {
         circle1.style.background = "#d94d4d";
+        circle1.classList.remove("neon-yellow", "neon-green");
+        circle1.classList.add("neon-red");;
     }
 
-// find circle 2
-    let circle2 = document.querySelector(".circle2");
+    let circle2 = document.querySelector("#serving-light")
 
-// lav condition til skift farve på cirkel 2
-    if (servingData < 2){
+    if (servingData == 1) {
         circle2.style.background = "#d94d4d";
-    } else if (servingData < 3 ){
+        circle2.classList.remove("neon-yellow", "neon-green");
+        circle2.classList.add("neon-red");
+    } else if (servingData == 2) {
         circle2.style.background = "#e79d3f";
-    } else if (servingData < 4 ){
+        circle2.classList.remove("neon-red", "neon-green");
+        circle2.classList.add("neon-yellow");
+    } else if (servingData == 3) {
         circle2.style.background = "#87ab66";
+        circle2.classList.remove("neon-yellow", "neon-red");
+        circle2.classList.add("neon-green");
     }
 };
 
