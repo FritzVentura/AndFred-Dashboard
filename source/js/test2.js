ddd99ccfb107e0ad7d94e2fa37e4399fcac2a894
Anders Verdier <Andersverdier@hotmail.com>
2018-06-11 09:13:08 +0200

    circle styling

--- source/js/test2.js
+++ source/js/test2.js
@@ -42,20 +42,17 @@ function circleStyling() {
 
     if (queueData < 5) {
         circle1.style.background = "#87ab66";
-        circle1.classList.remove("neon-yellow");
-        circle1.classList.remove("neon-red");
+        circle1.classList.remove("neon-yellow", "neon-red");
         circle1.classList.add("neon-green");
 
     } else if (queueData < 10) {
         circle1.style.background = "#e79d3f";
-        circle1.classList.remove("neon-green");
-        circle1.classList.remove("neon-red");
+        circle1.classList.remove("neon-red", "neon-green");
         circle1.classList.add("neon-yellow");
 
     } else if (queueData < 15) {
         circle1.style.background = "#d94d4d";
-        circle1.classList.remove("neon-yellow");
-        circle1.classList.remove("neon-green")
+        circle1.classList.remove("neon-yellow", "neon-green");
         circle1.classList.add("neon-red");;
     }
 
@@ -63,18 +60,15 @@ function circleStyling() {
 
     if (servingData < 2) {
         circle2.style.background = "#d94d4d";
-        circle2.classList.remove("neon-yellow");
-        circle2.classList.remove("neon-green");
+        circle2.classList.remove("neon-yellow", "neon-green");
         circle2.classList.add("neon-red");
     } else if (servingData < 3) {
         circle2.style.background = "#e79d3f";
-        circle2.classList.remove("neon-red");
-        circle2.classList.remove("neon-green");
+        circle2.classList.remove("neon-red", "neon-green");
         circle2.classList.add("neon-yellow");
     } else if (servingData < 4) {
         circle2.style.background = "#87ab66";
-        circle2.classList.remove("neon-yellow");
-        circle2.classList.remove("neon-red");
+        circle2.classList.remove("neon-yellow", "neon-red");
         circle2.classList.add("neon-green");
     }
 };
