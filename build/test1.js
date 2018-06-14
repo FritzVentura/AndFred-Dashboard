3bbd4f3bca684120705bb5baa696c6b0f8b06286
FrederikKofoed <fk.kofoed@gmail.com>
2018-06-14 10:45:11 +0200

    JS overført til MAIN i BUILD

--- build/test1.js
+++ build/test1.js
@@ -153,28 +153,31 @@ function tapInfo() {
     let tapStorage = jsonData.storage;
 
     //kombiner de to arrays med concat
-    let combiData = tapStorage.concat(tapData);
+   // let combiData = tapStorage.concat(tapData);
     /* 
        console.log("tap info", combiData)  */
 
-    tapData.forEach(element => {
-   
+       tapData.forEach(element => {
+        
+        
         let tapKlon = tapInfoTemplate.cloneNode(true).content;
 
         const newDiv = document.createElement("div");
+
+       
         newDiv.setAttribute("class", "liquid");
 
 
         tapKlon.querySelector(".tap-beer").textContent = element.beer;
         tapKlon.querySelector(".tap-level").textContent = (element.level*0.01) + " L.";
         tapKlon.querySelector(".tap-cap").textContent = (element.capacity*0.01) + " L.";
-
-        //tapKlon.querySelector(".tapsection").className = "hans" + i++
+/*         tapKlon.appendChild = newDiv;
+        tapKlon.appendChild = beerGlass; */
 
         newDiv.style.height = `${element.level*0.04}%` 
 
-        tapInfoContainer.appendChild(newDiv);
 
+          tapKlon.querySelector(".beer-glass").appendChild(newDiv)   
         tapInfoContainer.appendChild(tapKlon)
 
 
@@ -540,5 +543,5 @@ function customerList() {
 
 //--------------------------------------------------------------------
 // sæt interval
-window.setInterval(hentData, 10000);
+window.setInterval(hentData, 5000);
 hentData();
